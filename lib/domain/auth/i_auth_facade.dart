d041b86a843e6b30afc603cd93dffc4e3ec3bd57
Adriano Freires <freires.adriano@gmail.com>
2021-04-28 20:49:40 -0300

    Aula 3

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:firebase_ddd_resocoder/domain/auth/auth_failure.dart';
+import 'package:firebase_ddd_resocoder/domain/auth/value_objects.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
