a68daa77ecdfa9fe5903e109135837070b283a7c
Adriano Freires <freires.adriano@gmail.com>
2021-06-10 19:32:24 -0300

    Aula 14

---
+++ lib/infrastructure/notes/note_dtos.dart
@@ -0,0 +1,95 @@
+import 'dart:ui';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_ddd_resocoder/domain/core/value_objects.dart';
+import 'package:firebase_ddd_resocoder/domain/notes/note.dart';
+import 'package:firebase_ddd_resocoder/domain/notes/todo_item.dart';
+import 'package:firebase_ddd_resocoder/domain/notes/value_objects.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:kt_dart/kt.dart';
+
+part 'note_dtos.freezed.dart';
+part 'note_dtos.g.dart';
+
+@freezed
+abstract class NoteDto implements _$NoteDto {
+  const NoteDto._();
+
+  const factory NoteDto({
+    @JsonKey(ignore: true) String? id,
+    required String body,
+    required int color,
+    required List<TodoItemDto> todos,
+    @ServerTimestampConverter() required FieldValue serverTimeStamp,
+  }) = _NoteDto;
+
+  factory NoteDto.fromDomain(Note note) {
+    return NoteDto(
+      id: note.id.getOrCrash(),
+      body: note.body.getOrCrash(),
+      color: note.color.getOrCrash().value,
+      todos: note.todos
+          .getOrCrash()
+          .map(
+            (todoItem) => TodoItemDto.fromDomain(todoItem),
+          )
+          .asList(),
+      serverTimeStamp: FieldValue.serverTimestamp(),
+    );
+  }
+
+  Note toDomain() {
+    return Note(
+      id: UniqueID.fromUniqueString(id!),
+      body: NoteBody(body),
+      color: NoteColor(Color(color)),
+      todos: List3(todos.map((dto) => dto.toDomain()).toImmutableList()),
+    );
+  }
+
+  factory NoteDto.fromJson(Map<String, dynamic> json) =>
+      _$NoteDtoFromJson(json);
+
+  factory NoteDto.fromFirestore(DocumentSnapshot doc) {
+    return NoteDto.fromJson(doc.data() as Map<String, dynamic>)
+        .copyWith(id: doc.id);
+  }
+}
+
+class ServerTimestampConverter implements JsonConverter<FieldValue, Object> {
+  const ServerTimestampConverter();
+  @override
+  FieldValue fromJson(Object json) {
+    return FieldValue.serverTimestamp();
+  }
+
+  @override
+  Object toJson(FieldValue fieldValue) => fieldValue;
+}
+
+@freezed
+abstract class TodoItemDto with _$TodoItemDto {
+  const factory TodoItemDto({
+    required String id,
+    required String name,
+    required bool done,
+  }) = _TodoItemDto;
+
+  factory TodoItemDto.fromDomain(TodoItem todoItem) {
+    return TodoItemDto(
+      id: todoItem.id.getOrCrash(),
+      name: todoItem.name.getOrCrash(),
+      done: todoItem.done,
+    );
+  }
+
+  TodoItem toDomain() {
+    return TodoItem(
+      id: UniqueID.fromUniqueString(id),
+      name: TodoName(name),
+      done: done,
+    );
+  }
+
+  factory TodoItemDto.fromJson(Map<String, dynamic> json) =>
+      _$TodoItemDtoFromJson(json);
+}
