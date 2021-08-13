083ba22c0a7adf24258ea5ebcb4fdb4632638622
Adriano Freires <freires.adriano@gmail.com>
2021-08-13 12:13:09 -0300

    Aula 29

--- lib/presentation/notes/note_form/note_form_page.dart
+++ lib/presentation/notes/note_form/note_form_page.dart
@@ -8,6 +8,7 @@ import 'package:firebase_ddd_resocoder/presentation/notes/note_form/misc/todo_it
 import 'package:firebase_ddd_resocoder/presentation/notes/note_form/widgets/add_todo_tile_widget.dart';
 import 'package:firebase_ddd_resocoder/presentation/notes/note_form/widgets/body_field_widget.dart';
 import 'package:firebase_ddd_resocoder/presentation/notes/note_form/widgets/color_field_widget.dart';
+import 'package:firebase_ddd_resocoder/presentation/notes/note_form/widgets/todo_list_widget.dart';
 import 'package:firebase_ddd_resocoder/presentation/routes/router.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -149,6 +150,7 @@ class NoteFormPageScaffold extends StatelessWidget {
                   children: [
                     const BodyField(),
                     const ColorField(),
+                    const TodoList(),
                     const AddTodoTile(),
                   ],
                 ),
